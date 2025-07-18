cda8929abe5440b1ad6f02fa4535ff854ce0e00c
Jesica <jestsee@gmail.com>
2025-07-18 16:09:08 +0700

    update base url config

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,12 +1,5 @@
-import {
-  PUBLIC_VERCEL_ENV,
-  PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
-  PUBLIC_VERCEL_URL
-} from 'astro:env/client'
+import { PUBLIC_VERCEL_PROJECT_PRODUCTION_URL } from 'astro:env/client'
 
-const url =
-  PUBLIC_VERCEL_ENV === 'production'
-    ? PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-    : PUBLIC_VERCEL_URL
+const url = PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
 
 export const BASE_URL = url ? `https://${url}` : 'http://localhost:4321'
